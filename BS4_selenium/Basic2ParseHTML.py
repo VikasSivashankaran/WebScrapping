657d195739144ce29766ca1eb52418b44088558a
VikasSivashankaran <vikasbmca@gmail.com>
2024-09-26 16:15:32 +0530

    new

--- BS4_selenium/Basic2ParseHTML.py
+++ BS4_selenium/Basic2ParseHTML.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 
 url = "https://webscraper.io/test-sites/e-commerce/allinone/computers"
 r = requests.get(url)
-#print(r)
+print(r.status_code)
 soup = BeautifulSoup(r.text,"lxml")
-print(soup)
+# print(soup)
---
+++ BS4_selenium/Basic9BS4_with_pandas.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "lxml")
+names = soup.find_all("a", class_="title")
+# print(names)
+
+product_name = []
+for i in names:
+    name = i.text
+    product_name.append(name)
+print(product_name)
+
+prices = soup.find_all("h4",class_="price float-end card-title pull-right")
+price_list = []
+for i in prices:
+    price = i.text
+    price_list.append(price)
+print(price_list)
+
+Desc = soup.find_all("p",class_="description")
+desc_list = []
+for i in Desc:
+    des = i.text
+    desc_list.append(des)
+print(desc_list)
+
+Review = soup.find_all("p",class_="review-count")
+review_list = []
+for i in Review:
+    review = i.text
+    review_list.append(review)
+print(review_list)
